db926c60b34d46155735aec4f24ba8d955d27e23
moltencrux <moltencrux@gmail.com>
2025-08-12 22:43:42 +0800

    Adding in new preferences dialog and related code.
    Added:
      preferences UI design
      preferences dialog logic
      preference settings logic

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # compiles the UI and resource files form their definitions
+pyside6-rcc -o ui/wordlesmash_rc.py ui/wordlesmash.qrc
 pyuic6 -o ui/WordLeSmash_ui.py ui/WordLeSmash.ui
+pyuic6 -o ui/preferences_ui.py ui/preferences.ui
