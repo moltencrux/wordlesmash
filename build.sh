79bfb7c38450932836316bea93908a9763301d23
moltencrux <moltencrux@gmail.com>
2025-08-18 02:22:20 +0800

    Enabled copying profiles in Preferences.
    Profiles can now be duplicated in the preferences dialog.
    Other various fixes.

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pyuic6 -o ui/WordLeSmash_ui.py ui/WordLeSmash.ui
 pyuic6 -o ui/preferences_ui.py ui/preferences.ui
 pyuic6 -o ui/NewProfile_ui.py ui/NewProfile.ui
 pyuic6 -o ui/BatchAdd_ui.py ui/BatchAdd.ui
+pyuic6 -o ui/ProgressDialog_ui.py ui/ProgressDialog.ui
